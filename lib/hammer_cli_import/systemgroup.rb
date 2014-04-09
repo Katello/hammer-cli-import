d229373ba127ea367a055b17f1adb519c1e36006
Tomas Lestach <tlestach@redhat.com>
2014-04-09 10:10:40 +0200

    start using BaseCommand

--- lib/hammer_cli_import/systemgroup.rb
+++ lib/hammer_cli_import/systemgroup.rb
@@ -4,89 +4,23 @@ require 'apipie-bindings'
 
 module HammerCLIImport
   class Sat5Command
-    class SystemGroupImportCommand < HammerCLI::Apipie::Command
+    class SystemGroupImportCommand < BaseCommand
 
-      command_name "sg_import"
-      desc "Import system groups command"
+      command_name "system-group"
+      desc "Import system groups"
 
-      @@translate = {
-        "name" => "name",
-      }
+      persistent_maps :system_groups
 
-      option %w(--csv-file), 'FILE_NAME', 'CSV file'
-
-      def translate_dict(dict)
-        hash = Hash.new
-        @@translate.keys.each do | key |
-          hash[@@translate[key]] = dict[key]
-        end
-        return hash
+      def mk_sg_hash(data)
+          {
+            :name => data["name"],
+            :organization_id => lookup_entity(:organizations, data["org_id"])["label"],
+          }
       end
 
-      def csv_to_dict(csv_file)
-        csv = {}
-        puts "Reading: #{csv_file}"
-        CSV.foreach(csv_file || '/dev/stdin',
-          :headers => true, :converters => :all) do |row|
-          csv[row.fields[0]] = Hash[row.headers.zip(row.fields)]
-        end
-        return csv
-      end
-
-      def get_org_label(id)
-        org = @api.resource(:organizations).call(:show, {"id" => id})
-        puts "Translating organization id to label ..."
-        return org["label"]
-      end
-
-      def execute
-        @api = ApipieBindings::API.new({
-          :uri => HammerCLI::Settings.get(:foreman, :host),
-          :username => HammerCLI::Settings.get(:foreman, :username),
-          :password => HammerCLI::Settings.get(:foreman, :password),
-          :api_version => 2
-        })
-
-        sgs = csv_to_dict(option_csv_file)
-        sgs.each do | id, sg |
-          begin
-            exists = @api.resource(:system_groups).call(:show, {"id" => id})
-          # rescue ResourceNotFound
-          rescue => e
-            #puts "exception"
-            #puts e.message
-          end
-          # puts "exists: #{exists != nil}"
-          if exists == nil
-            params = translate_dict(sg)
-            puts params.inspect
-            params["organization_id"] = get_org_label(sg["org_id"])
-            new_sg = @api.resource(:system_groups).call(:create, params)
-            puts "Creating: #{new_sg.inspect}"
-          end
-        end
-
-        HammerCLI::EX_OK
-      end
-    end
-
-    class SystemGroupDeleteCommand < HammerCLI::Apipie::Command
-
-      command_name "sg_remove"
-      desc "Delete system groups command"
-
-      option %w(--id), 'ID', 'system group id'
-
-      def execute
-        puts "#{option_id}"
-        @api = ApipieBindings::API.new({
-          :uri => HammerCLI::Settings.get(:foreman, :host),
-          :username => HammerCLI::Settings.get(:foreman, :username),
-          :password => HammerCLI::Settings.get(:foreman, :password),
-          :api_version => 2
-        })
-        puts @api.resource(:system_groups).call(:destroy, {"id" => option_id})
-        HammerCLI::EX_OK
+      def import_single_row(data)
+        sg = mk_sg_hash data
+        create_entity(:system_groups, sg, data["group_id"])
       end
     end
     autoload_subcommands
