02da5e715fe1339835071be6782c7e14f146b2c2
Tomas Lestach <tlestach@redhat.com>
2014-04-16 16:11:22 +0200

    list_entities only if we've already imported some

--- import/lib/hammer_cli_import/base.rb
+++ import/lib/hammer_cli_import/base.rb
@@ -77,12 +77,14 @@ module HammerCLIImport
 
     def verify_maps()
       @pm.keys.each do |map_sym|
-        entities = list_entities map_sym
-        entity_ids = entities.map { |e| e["id"].to_i }
-        extra = @pm[map_sym].to_hash.values - entity_ids
-        unless extra.empty?
-          puts "Removing " + map_sym.to_s + " from persistent map: " + extra.join(" ")
-          @pm[map_sym].delete_if { |key, value| extra.include?(value) }
+        unless @pm[map_sym].to_hash.empty?
+          entities = list_entities map_sym
+          entity_ids = entities.map { |e| e["id"].to_i }
+          extra = @pm[map_sym].to_hash.values - entity_ids
+          unless extra.empty?
+            puts "Removing " + map_sym.to_s + " from persistent map: " + extra.join(" ")
+            @pm[map_sym].delete_if { |key, value| extra.include?(value) }
+          end
         end
       end
     end
