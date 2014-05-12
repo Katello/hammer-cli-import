82b87d58562688d5e82161e70f3b47cffc1013e1
Matej Kollar <mkollar@redhat.com>
2014-05-12 13:15:37 +0200

    Local repositories

---
+++ lib/hammer_cli_import/localrepos.rb
@@ -0,0 +1,66 @@
+# vim: autoindent tabstop=2 shiftwidth=2 expandtab softtabstop=2 filetype=ruby
+require 'hammer_cli'
+require 'uri'
+
+module HammerCLIImport
+  class ImportCommand
+    class LocalRepositoryImportCommand < BaseCommand
+      command_name 'local-repo'
+      desc 'Import local repositories.'
+
+      csv_columns 'org_id', 'channel_id', 'channel_label'
+
+      persistent_maps :organizations, :repositories
+      persistent_map :products, [{'org_id' => Fixnum}, {'label' => String}], ['sat6' => Fixnum]
+
+
+      option ['--sync'], :flag, 'Synchronize local repositories', :default => false
+      option ['--dir'], 'DIR', 'Export directory'
+
+      def directory
+        option_dir || File.dirname(option_csv_file)
+      end
+
+      #######
+      # -> DUPE
+      def mk_product_hash(data, product_name)
+        {
+          :name => product_name,
+          :organization_id => lookup_entity(:organizations, get_translated_id(:organizations, data['org_id'].to_i))['label']
+        }
+      end
+
+      def mk_repo_hash(data, product_id)
+        {
+          :name => "Local-repository-for-#{data['channel_label']}",
+          :product_id => product_id,
+          :url => 'file://' + File.join(directory, data['org_id'], data['channel_id']),
+          :content_type => 'yum'
+        }
+      end
+
+      def sync_repo(repo)
+        @api.resource(:repositories).call(:sync, {:id => repo['id']})
+      end
+      # <-
+      #######
+
+
+      def encode(a, b)
+        - (2 ** a) * (2*b + 1)
+      end
+
+      def import_single_row(data)
+        product_name = 'Local-repositories'
+        composite_id = [data['org_id'].to_i, product_name]
+        product_hash = mk_product_hash data, product_name
+        product_id = create_entity(:products, product_hash, composite_id)['id'].to_i
+
+        repo_hash = mk_repo_hash(data, product_id)
+        repo = create_entity(:repositories, repo_hash, encode(data['org_id'].to_i, data['channel_id'].to_i))
+
+        sync_repo repo if option_sync?
+      end
+    end
+  end
+end
