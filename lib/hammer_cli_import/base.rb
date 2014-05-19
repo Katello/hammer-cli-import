93416a2184b218627189060c48af8f5b7e3258be
Tomas Lestach <tlestach@redhat.com>
2014-05-19 10:24:05 +0200

    Bug 1084927 seems to be fixed, removing workaround
    part 2

--- lib/hammer_cli_import/base.rb
+++ lib/hammer_cli_import/base.rb
@@ -98,8 +98,7 @@ module HammerCLIImport
         results = []
         # check only entities in imported orgs (not all of them)
         @pm[:organizations].to_hash.values.each do |org_id|
-          org_identifier = lookup_entity(:organizations, org_id)['id']
-          entities = @api.resource(entity_type).call(:index, {'per_page' => 999999, 'organization_id' => org_identifier})
+          entities = @api.resource(entity_type).call(:index, {'per_page' => 999999, 'organization_id' => org_id})
           entities['results'].each do |entity|
             @cache[entity_type][entity['id']] = entity
           end
