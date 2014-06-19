35a7475ba27fd90cca33e60c95475e1b94512699
Matej Kollar <mkollar@redhat.com>
2014-06-19 14:19:26 +0200

    Licensing boilerplate

--- lib/hammer_cli_import/all.rb
+++ lib/hammer_cli_import/all.rb
@@ -1,3 +1,20 @@
+# vim: autoindent tabstop=2 shiftwidth=2 expandtab softtabstop=2 filetype=ruby
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This file is part of hammer-cli-import.
+#
+# hammer-cli-import is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# hammer-cli-import is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with hammer-cli-import.  If not, see <http://www.gnu.org/licenses/>.
 require 'hammer_cli'
 require 'hammer_cli_import'
 
