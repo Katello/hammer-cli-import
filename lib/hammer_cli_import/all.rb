753479213a464fa1471f78fc30c5f629002e26f7
Grant Gainey <ggainey@redhat.com>
2014-06-27 20:35:40 -0400

    Teach 'all' about new entities and new options

--- lib/hammer_cli_import/all.rb
+++ lib/hammer_cli_import/all.rb
@@ -29,11 +29,13 @@ module HammerCLIImport
       option ['--directory'], 'DIR_PATH', 'stargate-export directory', :default => '/tmp/exports'
       option ['--entities'], 'entity[,entity...]', 'Import specific entities', :default => 'all'
       option ['--list-entities'], :flag, 'List entities we understand', :default => false
+      option ['--into-org-id'], 'ORG_ID', 'Import all organizations into one specified by id'
+      option ['--merge-users'], :flag, 'Merge pre-created users (except admin)', :default => false
       option ['--dry-run'], :flag, 'Show what we would have done, if we\'d been allowed', :default => false
 
       # An ordered-list of the entities we know how to import
       class << self; attr_accessor :entity_order end
-      @entity_order = %w(organizations users host-collections repositories)
+      @entity_order = %w(organization user host-collection repository-enable repository content-view activation-key template-snippet)
 
       #
       # A list of what we know how to do.
@@ -43,30 +45,45 @@ module HammerCLIImport
       # It will look in ~/exports/<Sat5-export-name>.csv for data
       #
       class << self; attr_accessor :known end
-      @known = { 'host-collections' =>
+      @known = {
+                 'activation-key' =>
+                    {'export-file' => 'activation-keys',
+                     'import-class' => 'ActivationKeyImportCommand',
+                     'depends-on' => 'organization',
+                     'import' => false },
+                 'content-view' =>
+                    {'export-file' => 'CHANNELS/export',
+                     'import-class' => 'LocalRepositoryImportCommand',
+                     'depends-on' => 'repository',
+                     'import' => false },
+                 'repository' =>
+                    {'export-file' => 'repositories',
+                     'import-class' => 'RepositoryImportCommand',
+                     'depends-on' => 'organization',
+                     'import' => false },
+                 'host-collection' =>
                     {'export-file' => 'system-groups',
                      'import-class' => 'SystemGroupImportCommand',
-                     'depends-on' => 'organizations',
+                     'depends-on' => 'organization',
                      'import' => false },
-                 'organizations' =>
+                 'organization' =>
                     {'export-file' => 'users',
                      'import-class' => 'OrganizationImportCommand',
                      'depends-on' => '',
                      'import' => false },
-                 'users' =>
+                 'repository-enable' =>
+                    {'export-file' => 'channels',
+                     'import-class' => 'RepositoryEnableCommand',
+                     'depends-on' => 'organization',
+                     'import' => false },
+                 'template-snippet' =>
+                    {'export-file' => 'kickstart-scripts',
+                     'import-class' => 'TemplateSnippetImportCommand',
+                     'import' => false },
+                 'user' =>
                     {'export-file' => 'users',
                      'import-class' => 'UserImportCommand',
-                     'depends-on' => 'organizations',
-                     'import' => false },
-                 'repositories' =>
-                    {'export-file' => 'repositories',
-                     'import-class' => 'RepositoryImportCommand',
-                     'depends-on' => 'organizations',
-                     'import' => false },
-                 'custom-channels' =>
-                    {'export-file' => 'custom-channels',
-                     'import-class' => 'CustomChannelImportCommand',
-                     'depends-on' => 'organizations',
+                     'depends-on' => 'organization',
                      'import' => false }
                 }
 
@@ -83,6 +100,8 @@ module HammerCLIImport
         to_import = option_entities.split(',')
         AllCommand.known.each_key do |key|
           AllCommand.known[key]['import'] = (to_import.include?(key) || to_import.include?('all'))
+          next if AllCommand.known[key]['depends-on'].nil?
+
           depends_on = AllCommand.known[key]['depends-on'].split(',')
           depends_on.each do |entity_name|
             AllCommand.known[entity_name]['import'] = true
@@ -90,21 +109,51 @@ module HammerCLIImport
         end
       end
 
+      # Some subcommands have their own special args
+      # This is the function that will know them all
+      #
+      # 'user' needs --new-passwords and may need --merge-users
+      # 'organization' may need --into-org-id
+      # 'content-view needs --dir, and knows its own --csv-file in that dir
+      #
+      # TODO: add existence-check and throw if file doesn't exist
+      def build_args(key, filename)
+        args = ['--csv-file', filename]
+        case key
+        when 'organization'
+          args << '--into-org-id' << option_into_org_id unless option_into_org_id.nil?
+        when 'content-view'
+          args = ['--csv-file', "#{option_directory}/CHANNELS/export.csv"]
+          args << '--dir' << "#{option_directory}/CHANNELS"
+        when 'user'
+          pwd_filename = "passwords_#{Time.now.utc.iso8601}.csv"
+          args << '--new-passwords' << pwd_filename
+          args << '--merge-users' if option_merge_users?
+        end
+        return args
+      end
+
       # Do the import(s)
       def import_from
         AllCommand.entity_order.each do |key|
           a_map = AllCommand.known[key]
-          import_file = "#{option_directory}/#{a_map['export-file']}.csv"
-          if a_map['import']
-            puts "IMPORT #{key} FROM #{import_file}"
-            args = ['--csv-file', import_file]
 
-            #############################################################
-            # MAGIC! We create a class from the class-name-string here! #
-            #############################################################
-            import_class = HammerCLIImport::ImportCommand.const_get(a_map['import-class'])
-            unless option_dry_run?
-              import_class.new(args).run(args)
+          if a_map['import']
+            import_file = "#{option_directory}/#{a_map['export-file']}.csv"
+            # TODO: catch thrown error and skip with message
+            args = build_args(key, import_file)
+            puts sprintf("Import %-20s using %s", key, args.inspect)
+            if File.exist? import_file
+
+              #############################################################
+              # MAGIC! We create a class from the class-name-string here! #
+              #############################################################
+              import_class = HammerCLIImport::ImportCommand.const_get(a_map['import-class'])
+              unless option_dry_run?
+                import_class.new(args).run(args)
+              end
+            else
+              puts "...SKIPPING, no file #{import_file} available."
             end
           end
         end
@@ -113,11 +162,10 @@ module HammerCLIImport
       def execute
         if option_list_entities?
           do_list
-          exit 0
+        else
+          set_import_targets
+          import_from
         end
-
-        set_import_targets
-        import_from
         HammerCLI::EX_OK
       end
     end
